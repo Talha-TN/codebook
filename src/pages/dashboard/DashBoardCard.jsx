c5017759ab1022d42011506ffd5e7fe4ae66e73f
Talha-TN <nawazt964@gmail.com>
2024-08-30 12:17:34 -0700

    structure the base projects made all folders,pages and components

---
+++ src/pages/dashboard/DashBoardCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashBoardCard = () => {
+  return (
+    <div>DashBoardCard</div>
+  )
+}
+
+export default DashBoardCard
---
+++ src/pages/dashboard/DashBoardEmpty.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashBoardEmpty = () => {
+  return (
+    <div>DashBoardEmpty</div>
+  )
+}
+
+export default DashBoardEmpty
